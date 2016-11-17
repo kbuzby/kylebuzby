f77ffec1cc988e7e05a0fa2832159ca92e567fc8
Kyle Buzby <kyle.buzby@gmail.com>
2016-11-16 22:35:00 -0600

    added getting first posts on main index and minor fixes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
 
   def index
+    @carPost = Post.where(category: "Car").last
+    @photoPost = Post.where(category: "Photos").last
+    @techPost = Post.where(category: "Tech").last
   end
 
   def about
