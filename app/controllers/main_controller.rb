195572fe8cc33af1294af08dd770c74fdda104e9
Kyle Buzby <kyle.buzby@gmail.com>
2016-11-16 22:59:01 -0600

    optimized html code for panels

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,12 @@
 class MainController < ApplicationController
 
   def index
-    @carPost = Post.where(category: "Car").last
-    @photoPost = Post.where(category: "Photos").last
-    @techPost = Post.where(category: "Tech").last
+    carPost = Post.where(category: "Car").last
+    photoPost = Post.where(category: "Photos").last
+    techPost = Post.where(category: "Tech").last
+
+    @latestPosts = {"Car" => carPost, "Photo" => photoPost, "Tech" => techPost}
+
   end
 
   def about
